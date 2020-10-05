653c44a2ff847bec95b805743123936c4d824e35
Sander <sandervandenoetelaar@hotmail.com>
2020-10-05 11:11:12 +0200

    game layout

--- app/src/main/java/nl/svdoetelaar/madlevel4task2/ui/play/PlayFragment.kt
+++ app/src/main/java/nl/svdoetelaar/madlevel4task2/ui/play/PlayFragment.kt
@@ -1,13 +1,10 @@
 package nl.svdoetelaar.madlevel4task2.ui.play
 
-import android.app.Application
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import nl.svdoetelaar.madlevel4task2.R
 
 /**
@@ -20,7 +17,7 @@ class PlayFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_play, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
