17938d632af095d5841745fca40b2aaaaf3bd65c
Sander <sandervandenoetelaar@hotmail.com>
2020-10-05 16:09:56 +0200

    Added cheats and winner detection

---
+++ app/src/main/java/nl/svdoetelaar/madlevel4task2/model/Winner.kt
@@ -0,0 +1,7 @@
+package nl.svdoetelaar.madlevel4task2.model
+
+enum class Winner {
+    PLAYER,
+    DRAW,
+    COMPUTER
+}
--- app/src/main/java/nl/svdoetelaar/madlevel4task2/ui/PlayFragment.kt
+++ app/src/main/java/nl/svdoetelaar/madlevel4task2/ui/PlayFragment.kt
@@ -1,14 +1,20 @@
 package nl.svdoetelaar.madlevel4task2.ui
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import nl.svdoetelaar.madlevel4task2.R
 import nl.svdoetelaar.madlevel4task2.databinding.FragmentPlayBinding
 import nl.svdoetelaar.madlevel4task2.model.GameMode
 import nl.svdoetelaar.madlevel4task2.model.RockPaperScissors
+import nl.svdoetelaar.madlevel4task2.model.Winner
+import java.lang.Math.floorMod
 import kotlin.random.Random
 
 /**
@@ -17,8 +23,8 @@ import kotlin.random.Random
 class PlayFragment : Fragment() {
 
     private lateinit var binding: FragmentPlayBinding
-    private var mode = GameMode.RANDOM
-    private val rockPaperScissorsArray = RockPaperScissors.values()
+    private var gameMode = GameMode.RANDOM
+    private val aRPS = RockPaperScissors.values() //arrayOfRockPaperScissors (this is just shorter)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +35,7 @@ class PlayFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -40,22 +47,35 @@ class PlayFragment : Fragment() {
         binding.ivPaper.setOnClickListener { play(RockPaperScissors.PAPER) }
         binding.ivScissors.setOnClickListener { play(RockPaperScissors.SCISSORS) }
 
-
+        binding.ivYou.setOnLongClickListener { setCheat(GameMode.PLAYER) }
+        binding.tvVS.setOnLongClickListener { setCheat(GameMode.DRAW) }
+        binding.ivComputer.setOnLongClickListener { setCheat(GameMode.COMPUTER) }
     }
 
+    private fun setCheat(gameMode: GameMode): Boolean {
+        this.gameMode = if (this.gameMode != gameMode) {
+            gameMode
+        } else {
+            GameMode.RANDOM
+        }
+        Toast.makeText(requireContext(), "Set gamemode to ${this.gameMode}", Toast.LENGTH_SHORT).show()
+        return true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
     private fun play(playerRPS: RockPaperScissors) {
-        when (mode) {
+        when (gameMode) {
             GameMode.RANDOM -> {
                 decideWinner(
                     playerRPS,
-                    rockPaperScissorsArray[Random.nextInt(rockPaperScissorsArray.size)]
+                    aRPS[Random.nextInt(aRPS.size)]
                 )
             }
 
             GameMode.PLAYER -> {
                 decideWinner(
                     playerRPS,
-                    rockPaperScissorsArray[(rockPaperScissorsArray.indexOf(playerRPS) - 1 + rockPaperScissorsArray.size) % rockPaperScissorsArray.size]
+                    aRPS[floorMod((playerRPS.ordinal - 1), aRPS.size)]
                 )
             }
 
@@ -69,13 +89,46 @@ class PlayFragment : Fragment() {
             GameMode.COMPUTER -> {
                 decideWinner(
                     playerRPS,
-                    rockPaperScissorsArray[(rockPaperScissorsArray.indexOf(playerRPS) + 1) % rockPaperScissorsArray.size]
+                    aRPS[floorMod((playerRPS.ordinal + 1), aRPS.size)]
                 )
             }
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     private fun decideWinner(player: RockPaperScissors, computer: RockPaperScissors) {
-        Log.i("playFragment", "player: $player, computer: $computer")
+        val winner = when {
+            player == computer -> {
+                Winner.DRAW
+            }
+            floorMod((player.ordinal - 1), aRPS.size) == computer.ordinal -> {
+                Winner.PLAYER
+            }
+            else -> {
+                Winner.COMPUTER
+            }
+        }
+
+        binding.ivYou.setImageResource(getImageResource(player))
+        binding.ivComputer.setImageResource(getImageResource(computer))
+        binding.tvResult.setText(getResultString(winner))
+
+        Log.i("playFragmentGame", "player: $player, computer: $computer, winner $winner")
+    }
+
+    private fun getImageResource(rps: RockPaperScissors): Int {
+        return when (rps) {
+            RockPaperScissors.ROCK -> R.drawable.rock
+            RockPaperScissors.PAPER -> R.drawable.paper
+            RockPaperScissors.SCISSORS -> R.drawable.scissors
+        }
+    }
+
+    private fun getResultString(winner: Winner): Int {
+        return when (winner) {
+            Winner.PLAYER -> R.string.you_win
+            Winner.DRAW -> R.string.draw
+            Winner.COMPUTER -> R.string.computer_wins
+        }
     }
 }
